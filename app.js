9cc9f4a6c62cdced42d08e43e026b5130924ad31
mohammad irfan <fleadomgreen@gmail.com>
2025-05-14 15:18:24 +0700

    pushing counter play project

---
+++ app.js
@@ -0,0 +1,10 @@
+let count = 0;
+        document.getElementById('increment').addEventListener('click', ()=>{
+            count++;
+            document.getElementById('counter').textContent = count;
+        });
+
+        document.getElementById('decrement').addEventListener('click', ()=>{
+            count--;
+            document.getElementById('counter').textContent = count;
+        });
